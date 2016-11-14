4d51eb65a5994a4840da24543cfdaee4c2f66931
Will Avudim <will.avudim@gmail.com>
2016-11-13 20:55:41 -0800

    Start with classic vueify install.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  #app
+    h1 {{ msg }}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+</style>
